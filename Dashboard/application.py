c9360cf23276091f9ce6402bb6c70b13937fa435
Habibatou TOGOLA <107706712+habibatoutogola@users.noreply.github.com>
2022-09-15 10:28:19 +0200

    Update application.py

--- Dashboard/application.py
+++ Dashboard/application.py
@@ -88,7 +88,7 @@ st.write("**Education type :**", row_appli_sk['NAME_EDUCATION_TYPE'].values[0])
 st.write("**Occupation type :**", row_appli_sk['OCCUPATION_TYPE'].values[0])
 st.write("**Client owns a house or flat :**", row_appli_sk['FLAG_OWN_REALTY'].values[0])
 st.write("**Income of the client :**", row_appli_sk['AMT_INCOME_TOTAL'].values[0])
-st.write("**ANNUITY_CREDIT_PERCENT_INCOME : {:.2f}**".format(row_df_sk['ANNUITY_INCOME_PERC'].values[0]*100), "%")
+st.write("**ANNUITY_CREDIT_PERCENT_INCOME :**{:.2f}".format(row_df_sk['ANNUITY_INCOME_PERC'].values[0]*100), "%")
 #income_perc =row_df_sk['ANNUITY_INCOME_PERC'].values
 #st.write(f"Loan annuity / Income of the client : {income_perc*100:.2f} %")
 
@@ -96,8 +96,8 @@ st.subheader("Credit Information")
 st.write("**Contract type :**", row_appli_sk['NAME_CONTRACT_TYPE'].values[0])
 st.write("**Credit amount of the loan :**", row_appli_sk['AMT_CREDIT'].values[0])
 annuity =row_appli_sk['AMT_ANNUITY'].values[0] / 12
-st.write("**Loan monthly : {:.1f}**".format(annuity))
-st.write("**INCOME_CREDIT_PERC : {:.2f}**".format(row_df_sk['INCOME_CREDIT_PERC'].values[0]*100), "%")
+st.write("**Loan monthly :**{:.1f}".format(annuity))
+st.write("**INCOME_CREDIT_PERC :**{:.2f}".format(row_df_sk['INCOME_CREDIT_PERC'].values[0]*100), "%")
 #income_credit_perc =row_df_sk['INCOME_CREDIT_PERC'].values[0]
 #st.write(f"Income of the client / Credit amount of the loan : {income_credit_perc*100:.2f} %")
 
@@ -122,7 +122,7 @@ st.plotly_chart(fig)
 st.subheader("Feature importance")                           
 shap.initjs()   
 X=df[df['SK_ID_CURR']==int(id_client)]
-X.drop(columns=['SK_ID_CURR'],inplace=True)
+X.TARGET=df_TAREGT
             
 fig, ax = plt.subplots(figsize=(10, 10))
 explainer = shap.TreeExplainer(grid_lgbm)
